7f566300a0cd9f158b2c883a490f986e733fdad9
ThomasKyle <thomas19992010@hotmail.com>
2021-02-03 18:04:39 -0500

    added question functionality

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+inquirer
+.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+  }
+])
+.then(answers => console.log(answers));
+
+const pageHTML = generatePage(name, github);
+
+  fs.writeFile('index.html', generatePage(name, github), err=> {
+      if (err) throw err;
+
+      console.log('Portfolio complete! Check out index.html to see the output!')
+  });
